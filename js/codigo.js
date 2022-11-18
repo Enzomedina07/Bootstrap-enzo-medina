fb57637ade846b4c4fa5a3ce6afb56fd492d3587
enzo medina <enzomedina0798@gmail.com>
2022-11-18 14:43:38 -0300

    Termine el carrito en el product

--- js/codigo.js
+++ js/codigo.js
@@ -1,89 +1,149 @@
 //Carrito
-const carro = [];
-
-//ARRAY DE PRODUCTOS
-
-const productos = [{ id: 1,  producto: "Blue dice", precio: 1500 },
-                {  id: 2,  producto: "Gold dice", precio: 1500 },
-                {  id: 3,  producto: "Beholder"  , precio: 500},
-                {  id: 4,  producto: "Metal dice" , precio: 3000}];
-
-//hacemos un let de los botones para agregar al carrito
-
-let boton1 = document.getElementById("1")
-let boton2 = document.getElementById("2")
-let boton3 = document.getElementById("3")
-let boton4 = document.getElementById("4")
-boton1.onmouseover = () =>{
-    console.log("Queres comprar un d20, son buenos");
-    boton1.className="btn btn-danger"
-}
-boton1.onmouseout = () =>{
-    boton1.className="btn btn-primary btn-sm btncarrito"
-}
-boton2.onmouseover = () =>{
-    console.log("Queres comprar un d20, son buenos");
-    boton2.className="btn btn-danger"
-}
-boton2.onmouseout = () =>{
-    boton2.className="btn btn-primary btn-sm btncarrito"
-}
-boton3.onmouseover = () =>{
-    console.log("Queres comprar esta mini, es genial");
-    boton3.className="btn btn-danger"
-}
-boton3.onmouseout = () =>{
-    boton3.className="btn btn-primary btn-sm btncarrito"
-}
-boton4.onmouseover = () =>{
-    console.log("Queres comprar un d20, son buenos");
-    boton4.className="btn btn-danger"
-}
-boton4.onmouseout = () =>{
-    boton4.className="btn btn-primary btn-sm btncarrito"
+let carro = JSON.parse(localStorage.getItem("carrito")) || [];
+let totalCarrito
+let btnCompra = document.getElementById("btncompra")
+let cartas = document.getElementById("cartas")
+//ARRAY DE PRODUCTOS que saco desde el JSON
+let prodJSON=[] //array
+//
+//
+if(carro.lenght !=0){
+    console.log("Retrieving Cart")
+    dibujarCarrito()
+    obtenerproductos();
 }
 
-boton1.onclick = () => {
-    alert("Agregaste Blue dice set al carro");
-    carro.push(productos[0]);
-    console.log(carro)
+//Funcion para dibujar el carrito viejo
+function dibujarCarrito(){
+    for(const producto of carro){
+        document.getElementById("carrito").innerHTML +=`
+    <tr>
+        <td>${producto.id}</td>
+        <td>${producto.producto}</td>
+        <td>${producto.precio}</td>
+        <td><button class="btn btn-light" onclick="eliminar(event)">Eliminar</button></td>
+        </tr>
+    `;
 }
-boton2.onclick = () => {
-    alert("Agregaste Gold dice set al carro");
-    carro.push(productos[1]);
-    console.log(carro)
-}
-boton3.onclick = () => {
-    alert("Agregaste Beholder mini al carro");
-    carro.push(productos[2]);
-    console.log(carro)
-}
-boton4.onclick = () => {
-    alert("Agregaste Metal dice set al carro");
-    carro.push(productos[3]);
-    console.log(carro)
+    totalCarrito = carro.reduce((acumulador,producto)=> acumulador + producto.precio,0);
+    let info = document.getElementById("total");
+    //agregar carro al storage
+    info.innerText="Total amount $: "+totalCarrito
 }
 
-//almacenar productos en el storage
-const guardarLocal = (clave, valor) => { localStorage.setItem(clave, valor) };
-//Almacenar producto por producto
-guardarLocal("listaProductos", JSON.stringify(productos));
-class Producto {
-    constructor(obj) {
-        this.nombre  = obj.producto.toUpperCase();
-        this.precio  = parseFloat(obj.precio);
+
+//Funcionar para renderizar los productos en DOM
+function renderizar(){
+    for(const producto of prodJSON){
+        cartas.innerHTML += `
+        <div class="col">
+                <div class="card text-white bg-info mb-3">
+                    <img src="${producto.foto}" class="card-img-top" alt="card-grid-image">
+                    <div class="card-body">
+                    <h5 class="card-title">${producto.producto}</h5>
+                    <p class="card-text">$${producto.precio}</p>
+                    <button class="btn btn-outline-danger btn-sm" id="btn${producto.id}">
+                        <p>Add to cart</p>
+                    </div>
+                </div>
+                </div>
+        `;
     }
-    sumaIva() {
-        this.precio = this.precio * 1.21;
+
+//evento para agregar
+    prodJSON.forEach(producto => {
+        //asi creamos un evento para cada boton
+        document.getElementById(`btn${producto.id}`).addEventListener("click",function(){
+            agregarCarrito(producto)
+        })
+    })
+}
+
+//funcionar para agregar al carrito
+function agregarCarrito(productoComprado){
+    carro.push(productoComprado);
+    console.table(carro);
+    //uso sweet alert para hacer las notificaciones al usuario
+    Swal.fire({
+        title: productoComprado.nombre,
+        text: 'Agregado al carrito',
+        imageUrl: productoComprado.foto,
+        imageWidth: 225,
+        imageHeight: 300,
+        imageAlt: productoComprado.producto,
+        showConfirmButton: false,
+        timer: 1500
+    })
+    document.getElementById("carrito").innerHTML += `
+        <tr>
+            <td>${productoComprado.id}</td>
+            <td>${productoComprado.producto}</td>
+            <td>${productoComprado.precio}</td>
+            <td><button class="btn btn-light" onclick="eliminar(event)">Eliminar</button></td>
+        </tr>
+    `;
+    totalCarrito = carro.reduce((acumulador,producto)=> acumulador + producto.precio,0);
+    let info = document.getElementById("total");
+    //agregar carro al storage
+    info.innerText="Total amount $: "+totalCarrito
+    localStorage.setItem("carrito",JSON.stringify(carro));
+}
+
+//Borrar productos del carrito
+function eliminar(ev){
+    console.log(ev);
+    let fila = ev.target.parentElement.parentElement;
+    console.log(fila);
+    let id = fila.children[0].innerText;
+    console.log(id);
+    let indice = carro.findIndex(producto => producto.id == id);
+    console.log(indice)
+    //remueve el producto del carro
+    carro.splice(indice,1);
+    console.table(carro);
+    //remueve la fila del carrito
+    fila.remove();
+    //recalcula el total a pagar
+    let preciosAcumulados = carro.reduce((acumulador,producto)=>acumulador+producto.precio,0);
+    total.innerText="Total a pagar $: "+preciosAcumulados;
+    //lo pasamos al storage
+    localStorage.setItem("carrito",JSON.stringify(carro))
+}
+
+//Traer productos JSON
+async function obtenerproductos(){ //llamar el JSON es un proceso asyncronico
+    const URLJSON="../js/productos.json";
+    const resp=await fetch(URLJSON); //por eso pongo un wait dps de las respuestas
+    const data=await resp.json(); // convierto la respuesta en data
+    prodJSON = data; //asigno la data al array de productos que declaramos arriba
+    renderizar(); //renderizamos las cartas
+}
+
+//Terminar la compra
+btnCompra.onclick = () =>{
+    if(carro.length==0){
+        Swal.fire({
+            title: "Empty Cart",
+            text: 'Make a death saving throw or buy something',
+            icon: 'error',
+            showConfirmButton: false,
+            timer: 3500
+    })
+    }else{
+        carro = [];
+        document.getElementById("carrito").innerHTML="";
+        let info = document.getElementById("total");
+        info.innerText="Total a pagar $: ";
+        Swal.fire({
+            title: "You succeeded your skill check",
+            text: 'Our wizard will send you an eagle in the next hours with your dices',
+            icon: 'success',
+            showConfirmButton: false,
+            timer: 4500
+        })
+    //Sacamos el carrito que ya se compro supuestamente
+    localStorage.removeItem("carrito");
     }
 }
-//Obtenemos el listado de productos almacenado
-const almacenados = JSON.parse(localStorage.getItem("listaProductos"));
-const productos2 = [];
-//Iteramos almacenados con for...of para transformar todos sus objetos a tipo producto.
-for (const objeto of almacenados)
-    productos2.push(new Producto(objeto));
-//Ahora tenemos objetos productos y podemos usar sus métodos
-for (const producto of productos2)
-producto.sumaIva();
-console.log(productos2)
+
+
